fc7ce506d993dc9a9379d1162ff76ca7af6f1417
Willian Galvani <williangalvani@gmail.com>
2019-04-23 17:11:26 -0300

    Add instructions of how to get apk

--- scripts/build-qgc.sh
+++ scripts/build-qgc.sh
@@ -9,4 +9,7 @@ make install INSTALL_ROOT=dist
 androiddeployqt --input android-libQGroundControl.so-deployment-settings.json --output dist/ --android-platform android-21 --deployment bundled --gradle --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /tmp/build/selfsigned.keystore dist/build/outputs/apk/dist-release-unsigned.apk qgc-self-signed -storepass potato
 mv /home/user/qgroundcontrol/build/dist/build/outputs/apk/dist-release-unsigned.apk /home/user/qgroundcontrol.apk
-
+echo "Build done"
+echo "exit and run"
+echo "docker cp \$(docker ps -alq):/home/user/qgroundcontrol.apk ."
+echo "to copy .apk to your current directory"
