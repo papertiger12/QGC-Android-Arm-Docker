5a59f0e5ad8ee2f2b041345790692a61f862b736
Willian Galvani <williangalvani@gmail.com>
2021-07-22 17:49:58 -0300

    fixup! Fetch gstreamer packages as done in qgc repo

--- scripts/install-gstreamer.sh
+++ scripts/install-gstreamer.sh
@@ -3,7 +3,7 @@
 
 mkdir /tmp/android
 cd /tmp/android
-wget --quiet https://qgroundcontrol.s3-us-west-2.amazonaws.com/dependencies/gstreamer-1.0-android-universal-1.18.1.tar.xz
+wget https://qgroundcontrol.s3-us-west-2.amazonaws.com/dependencies/gstreamer-1.0-android-universal-1.18.1.tar.xz
 mkdir gstreamer-1.0-android-universal-1.18.1
 tar xf gstreamer-1.0-android-universal-1.18.1.tar.xz -C gstreamer-1.0-android-universal-1.18.1
 rm gstreamer-1.0-android-universal-1.18.1.tar.xz
